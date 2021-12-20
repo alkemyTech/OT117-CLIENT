394cea286b6a65f63a8c6ede96ade93f918ca3f4
Dante Carrasco <arielcarp1996@gmail.com>
2021-12-20 14:27:37 -0300

    Modificando el reducer de autenticacion

--- src/app/authReducer/authReducer.js
+++ src/app/authReducer/authReducer.js
@@ -1,41 +1,41 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     isLogged: false,
     token: localStorage.getItem('token')??"",
     userData: {}
-};
+}
 
-const simpleState = key => (state, { payload }) => {
-    state[key] = payload;
-};
+const simpleState = (key) => (state, { payload }) => {
+    state[key] = payload
+}
 
-const localState = key => (state, { payload }) => {
-    localStorage.setItem([key], payload);
-    state[key] = payload;
-};
+const localState = (key) => (state, { payload }) => {
+    localStorage.setItem([key], payload)
+    state[key] = payload
+}
 
 const authSlice = createSlice({
-    name:"authReducer",
+    name: "authReducer",
     initialState,
     reducers: {
         logout: () => {
-            state.isLogged = false;
-            state.token = "";
-            localStorage.removeItem('token');
-            state.userData = {};
+            state.isLogged = false
+            state.token = ""
+            localStorage.removeItem('token')
+            state.userData = {}
         },
         setUserData: simpleState('userData'),
         setToken: localState('token'),
         setIsLogged: simpleState('isLogged')
     }
-});
+})
 
 export const {
     logout,
     setToken,
     setUserData,
     setIsLogged
-} = authSlice.actions;
+} = authSlice.actions
 
-export default authSlice.reducer;
+export default authSlice.reducer
