bbcffd697dfa781d6931ef1aab5b221e7d12533a
Axel Nieto <axelnieto616@gmail.com>
2021-12-30 23:54:00 -0300

    header fixed

--- src/Components/Layout/Header/HeaderLinks.js
+++ src/Components/Layout/Header/HeaderLinks.js
@@ -4,6 +4,8 @@ export const navLinks = [
     {text: 'Contacto', link: '/contacto', private:false, active:false},
     {text: 'Actividades', link: '/activities', private:false, active:false},
     {text: 'Novedades', link: '/novedades', private:false, active:false},
+]
+export const campaignLinks = [
     {text: 'Campaña escolar', link: '/school-campaign', private:false, active:false},
     {text: 'Campaña de juguetes', link: '/toys-campaign', private:false, active:false}
 ]
@@ -13,5 +15,11 @@ export const manageLinkActivation = (pathname) => {
             navLinks[index].active = true;
         else if(navLink.active == true)
             navLinks[index].active = false;
+    });
+    campaignLinks.forEach((campLink, index) => {
+        if(campLink.link == pathname)
+            campaignLinks[index].active = true;
+        else if(campLink.active == true)
+            campaignLinks[index].active = false;
     })
 }
--- src/Components/Layout/Header/NavLinksList.js
+++ src/Components/Layout/Header/NavLinksList.js
@@ -1,13 +1,24 @@
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import Collapse from '@mui/material/Collapse';
 import { Link } from 'react-router-dom';
+import React,{useState} from "react";
 
-const NavLinksList = ({navLinks, isLogged, horizontal}) => {
+const NavLinksList = ({navLinks,campaignLinks, isLogged, horizontal}) => {
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+        setOpen(!open);
+    };
     return (
         <>
         {horizontal ?
-            <List sx={{display:{xs:'none', lg:'flex'}}}>
+            <List sx={{display:{xs:'none', lg:'flex'}}}
+            component="nav"
+            >
                 {navLinks.map((navLink) => (
                     navLink.private ? (isLogged ??
                 <ListItem component={Link} to={navLink.link} sx={{minWidth:'fit-content'}} button key={navLink.text}>
@@ -17,6 +28,23 @@ const NavLinksList = ({navLinks, isLogged, horizontal}) => {
                     <ListItemText primary={navLink.text} sx={navLink.active ? {borderBottom:'2px solid #ec4c4c'} : {}}/>
                   </ListItem>
                 ))}
+                <div display="flex" flexDirection="column">
+                    <ListItem onClick={handleClick}>
+                        <ListItemText primary='Campañas'/>
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItem>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                            {campaignLinks.map((campLink) => (
+                                campLink.private ? (isLogged ??
+                            <ListItem component={Link} to={campLink.link} sx={{minWidth:'fit-content'}} button key={campLink.text}>
+                                <ListItemText primary={campLink.text} sx={campLink.active ? {borderBottom:'2px solid #ec4c4c'} : {}}/>
+                            </ListItem>)
+                            : <ListItem component={Link} to={campLink.link} sx={{minWidth:'fit-content'}} button key={campLink.text}>
+                                <ListItemText primary={campLink.text} sx={campLink.active ? {borderBottom:'2px solid #ec4c4c'} : {}}/>
+                            </ListItem>
+                            ))}
+                    </Collapse>
+                </div>
             </List>
             :<List>
             {navLinks.map((navLink) => (
@@ -28,6 +56,23 @@ const NavLinksList = ({navLinks, isLogged, horizontal}) => {
                       <ListItemText primary={navLink.text} sx={navLink.active ? {borderBottom:'2px solid #1a76d2'} : {}}/>
                     </ListItem>
               ))}
+              <div display="flex" flexDirection="column">
+                    <ListItem onClick={handleClick}>
+                        <ListItemText primary='Campañas'/>
+                        {open ? <ExpandLess /> : <ExpandMore />}
+                    </ListItem>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                            {campaignLinks.map((campLink) => (
+                                campLink.private ? (isLogged ??
+                            <ListItem component={Link} to={campLink.link} sx={{minWidth:'fit-content'}} button key={campLink.text}>
+                                <ListItemText primary={campLink.text} sx={campLink.active ? {borderBottom:'2px solid #ec4c4c'} : {}}/>
+                            </ListItem>)
+                            : <ListItem component={Link} to={campLink.link} sx={{minWidth:'fit-content'}} button key={campLink.text}>
+                                <ListItemText primary={campLink.text} sx={campLink.active ? {borderBottom:'2px solid #ec4c4c'} : {}}/>
+                            </ListItem>
+                            ))}
+                    </Collapse>
+                </div>
             </List>
         }
         </>
