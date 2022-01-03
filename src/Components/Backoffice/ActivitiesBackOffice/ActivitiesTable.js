b3fa718bf73e4f9d2f4e997f2c8bc48e23baaf1c
Dante Carrasco <arielcarp1996@gmail.com>
2022-01-02 21:17:48 -0300

    Adding fade effect in ActivitiesTable component

--- src/Components/Backoffice/ActivitiesBackOffice/ActivitiesTable.js
+++ src/Components/Backoffice/ActivitiesBackOffice/ActivitiesTable.js
@@ -4,35 +4,38 @@ import "../../../Styles/TableStyle.css";
 import ActivitiesTableRows from "../../Activities/ActivitiesTableRows";
 import {Table,TableBody,TableCell,TableContainer,TableHead,Button,TableRow} from "@mui/material";
 import TitleBackoffice from '../TitleBackoffice'
+import { Fade } from "react-awesome-reveal";
 const ActivitiesTable = () => {
   return (
     <>
      <TitleBackoffice title={"Edición de Actividades"} />
-      <TableContainer className="TableContainer">
-      <Table className="TableFinal">
-        <TableHead className="TableRowModify">
-          <TableRow>
-            <TableCell className="TableCell"align="center">Name</TableCell>
-            <TableCell className="TableCell"align="center">Image</TableCell>
-            <TableCell className="TableCell"align="center">CreatedAt</TableCell>
-            <TableCell align="center" className="TableCell">
-                  <Button
-                    color="buttoncreatenews"
-                    variant="contained"
-                    size="small"
-                    component={Link}
-                    to="/backoffice/activities/create"
-                  >
-                    Create Activity
-                  </Button>
-                </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <ActivitiesTableRows></ActivitiesTableRows>
-        </TableBody>
-      </Table>
-      </TableContainer>
+     <Fade duration="2000">
+        <TableContainer className="TableContainer">
+        <Table className="TableFinal">
+          <TableHead className="TableRowModify">
+            <TableRow>
+              <TableCell className="TableCell"align="center">Name</TableCell>
+              <TableCell className="TableCell"align="center">Image</TableCell>
+              <TableCell className="TableCell"align="center">CreatedAt</TableCell>
+              <TableCell align="center" className="TableCell">
+                    <Button
+                      color="buttoncreatenews"
+                      variant="contained"
+                      size="small"
+                      component={Link}
+                      to="/backoffice/activities/create"
+                    >
+                      Create Activity
+                    </Button>
+                  </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <ActivitiesTableRows></ActivitiesTableRows>
+          </TableBody>
+        </Table>
+        </TableContainer>
+     </Fade>
     </>
   );
 };
