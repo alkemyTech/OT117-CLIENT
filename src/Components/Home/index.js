cdf2a1b884357f1ffa3fb971593de4b98a83d1db
Dante Carrasco <arielcarp1996@gmail.com>
2022-01-06 16:22:53 -0300

    Show comments when user scrolls to that section in index component

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,19 +1,15 @@
 
 import React, { useEffect, useState } from 'react';
 import Carousel from '../Carousel/Carousel';
-import Footer from '../Footer/Footer';
 import { getOrganizationInformation } from '../../Services/OrganizationInformation';
-import Header from '../Layout/Header/Header';
 import CardsSection from './CardsSection';
 import * as newsService from '../../Services/newsServices';
 import * as testimonialService from '../../Services/testimonialService';
 import { errorMessage } from '../error';
-import SkeletonLoader from '../Loader/SkeletonLoader';
-import LoadingSpinner from '../../Utils/loadingSpinner';
-import { ConfirmAlert, InfoAlert } from '../common/alerts/Alerts';
 
 const Home = () => {
   const [welcomeText, setWelcomeText] = useState("");
+  const [sectionScroll, setSectionScroll] = useState(false)
   useEffect(() => {
     getOrganizationInformation()
       .then((res) => setWelcomeText(res.data.welcome_text))
@@ -21,6 +17,21 @@ const Home = () => {
         errorMessage(err);
       });
   }, []);
+  useEffect(() => {
+    const showTestimonial = () => {
+      const bodyHeight = document.documentElement.getBoundingClientRect().height
+      const scrollTop = document.documentElement.scrollTop
+      if (scrollTop >= bodyHeight / 1.8)
+      {
+        setSectionScroll(true)
+      }
+    }
+    window.addEventListener("scroll", showTestimonial)
+
+    return () => {
+      window.removeEventListener("scroll", showTestimonial)
+    }
+  }, [])
   return (
     <>
       <h1 style={{ textAlign: "center" }}>{welcomeText}</h1>
@@ -29,7 +40,7 @@ const Home = () => {
           maxWidth: "1400px",
           margin: "0 auto",
         }}
-      >
+        >
         <Carousel />
         <CardsSection
           title="Últimas novedades"
@@ -37,12 +48,14 @@ const Home = () => {
           getInformation={newsService.getAll}
           slices={3}
           button={{ text: "Ver todas", to: "/novedades" }}
-        />
-        <CardsSection
-          title="Testimonios"
-          getInformation={testimonialService.getAllTestimonial}
-          slices={3}
-        />
+          />
+        {sectionScroll &&
+          <CardsSection
+            title="Testimonios"
+            getInformation={testimonialService.getAllTestimonial}
+            slices={3}
+          />
+        }
       </section>
     </>
   );
