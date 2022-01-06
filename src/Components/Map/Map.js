9624ea28daf7df619656621e5ebfd9c4fac465f4
Dante Carrasco <arielcarp1996@gmail.com>
2022-01-05 23:01:00 -0300

    Modifying contact form styles

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,10 +1,13 @@
 import React from "react";
-import axios from "axios";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { mapToStyles } from "@popperjs/core/lib/modifiers/computeStyles";
+import styled from "styled-components";
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
 const libraries = ["places"]
-
+const MapContainer = styled.div`
+  --container-padding: 1rem;
+  height: 20rem;
+  width: 100%;
+`
 const Map = ({ coordinates }) => {
   const { isLoaded, loadError} = useLoadScript({
     id: "alchemy-location",
@@ -16,8 +19,8 @@ const Map = ({ coordinates }) => {
   if (!isLoaded || coordinates.length <= 0) return <p>Cargando Mapa....</p>
 
   return (
-    <div style={{ height: "60vh", width: "620px", padding: "20px" }}>
-      <GoogleMap
+    <MapContainer>
+      {/* <GoogleMap
         mapContainerStyle={{
           height: "100%",
           width: "100%",
@@ -32,12 +35,13 @@ const Map = ({ coordinates }) => {
           zoomControl: true
         }}
       >
-        <Marker position={{
-          lat: coordinates[0],
-          lng: coordinates[1]
-        }} />
-      </GoogleMap>
-    </div>
+        <Marker
+          position={{
+            lat: coordinates[0],
+            lng: coordinates[1]
+          }} />
+      </GoogleMap> */}
+    </MapContainer>
   )
 };
 
