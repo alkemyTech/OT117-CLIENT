648f3054d3163086dfd8d6defc5e207b462100ad
sntmsc <santiago.hamber@gmail.com>
2022-01-06 09:34:31 -0300

    AboutUs fixed, add social media content

--- src/Components/About/AboutUs.js
+++ src/Components/About/AboutUs.js
@@ -2,9 +2,9 @@ import Title from "../Title/Title";
 import { Box, Grid, Container, Typography } from "@mui/material";
 import "../../Styles/Container.css";
 import {
-  LinkedinCompanyProfile,
   LinkedinFollowCompany,
   TwitterTweet,
+  TwitterButton
 } from "react-social-plugins";
 import AboutUsMembers from "./AboutUsMembers";
 import nosotros from "../../Assets/TitleImages/nosotros.jpg";
@@ -13,28 +13,18 @@ import nosotrossvg from "../../Assets/waves/nosotros.svg";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import "../../Styles/AboutLinks.css"
 
 const AboutUs = () => {
-  useEffect(() => {
-    AOS.init();
-    AOS.refreshHard();
-    return () => {};
-  }, []);
-  AOS.init();
+
   const description = `Somos un centro comunitario que acompaña a más de 700 personas a través de las áreas de:
         Educación, deportes, primera infancia, salud, alimentación y trabajo social.`;
   return (
     <>
-      <Box className="title-container aos init">
         <Title title="NOSOTROS" image={nosotros} />
         <div className="title-container-text"></div>
-      </Box>
       <div className="ContenedorNosotros">
         <Box
-          data-aos="flip-left"
-          data-aos-duration="3000"
-          data-aos-delay="500"
-          component="img"
           sx={{
             height: 233,
             width: 350,
@@ -45,9 +35,6 @@ const AboutUs = () => {
           src={nosotrossvg}
         ></Box>
         <div
-          data-aos="fade-up"
-          data-aos-duration="2000"
-          data-aos-delay="500"
           className="ContenedorNosotrosH1"
         >
           <Box
@@ -67,15 +54,6 @@ const AboutUs = () => {
         <AboutUsMembers />
       </div>
       <div>
-        {/* <div
-            className="badge-base LI-profile-badge"
-            data-locale="es_ES"
-            data-size="large"
-            data-theme="light"
-            data-type="HORIZONTAL"
-            data-vanity="corporación-somos-más-68737437"
-            data-version="v1"
-          ></div> */}
         <div>
           <Container
             sx={{
@@ -87,6 +65,55 @@ const AboutUs = () => {
               mt: 9,
             }}
           >
+             <Typography
+              className="ContenedorTitulo"
+              variant="h3"
+              sx={{
+                fontSize: { lg: "45px", xs: "35px" },
+                bottom: "40px",
+                textTransform: "uppercase",
+                fontWeight: "600",
+                letterSpacing: "0.5rem",
+              }}>
+                REDES
+            </Typography>
+            <Box sx={{
+              margin:3,
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection:{xs:"column",md:"row"}
+            }}>
+              <a className="AboutLinks" style={{color:"#ec4c4c"}}
+              href="https://www.facebook.com/somos_mas">
+              Facebook</a>
+              <a className="AboutLinks" style={{color:"#f1bf67"}}
+              href="https://www.linkedin.com/company/somosmas">
+              Linkedin</a>
+              <a className="AboutLinks" style={{color:"#1d9bf0"}}
+              href="https://www.instagram.com/somosmas">
+              Instagram</a>
+            </Box>
+            <Box sx={{
+              margin:3
+            }}>
+              <LinkedinFollowCompany
+                companyId={3144678}
+                counter="top"
+                lang="en_US"
+              />
+            </Box>
+            <Box sx={{
+              margin:3
+            }}>
+              <TwitterButton
+                target="@somosmas"
+                text="es una ONG que vale la pena conocer"
+                type="Mention"
+                  />
+            </Box>
+
             <Grid container sx={{ m: 3 }}>
               <TwitterTweet
                 align="center"
